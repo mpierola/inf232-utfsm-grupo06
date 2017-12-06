cf059f92e304b5646d40140bdc3c8b8ab43a0195
mpierola <marcelo.pierola.13@sansano.usm.cl>
2017-12-05 22:19:26 -0300

    changed wsgi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 from django.core.wsgi import get_wsgi_application
