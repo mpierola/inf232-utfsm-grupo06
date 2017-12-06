42cf4f6b5646ba620c74ca379895e67f799f7730
mpierola <marcelo.pierola.13@sansano.usm.cl>
2017-12-05 23:23:28 -0300

    change Dockerfile

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-path = '/code'
+path = '/code/'
 if path not in sys.path:
     sys.path.append(path)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
