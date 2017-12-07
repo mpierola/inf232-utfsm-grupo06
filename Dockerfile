0463b8ef67b8aad6786b9df72fa0551bb6dcc8e0
mpierola <marcelo.pierola.13@sansano.usm.cl>
2017-12-06 21:17:55 -0300

    seems like everything is working now

--- Dockerfile
+++ Dockerfile
@@ -45,7 +45,7 @@ EXPOSE 8000
 ENV UWSGI_VIRTUALENV=/venv UWSGI_WSGI_FILE=mysite/wsgi.py UWSGI_HTTP=:8000 UWSGI_MASTER=1 UWSGI_WORKERS=2 UWSGI_THREADS=8 UWSGI_UID=1000 UWSGI_GID=2000 UWSGI_LAZY_APPS=1 UWSGI_WSGI_ENV_BEHAVIOR=holy
 
 # Call collectstatic (customize the following line with the minimal environment variables needed for manage.py to run):
-RUN DATABASE_URL=none /venv/bin/python manage.py collectstatic --noinput
+RUN DATABASE_URL=postgres://app_user:changeme@db/app_db  /venv/bin/python manage.py collectstatic --noinput
 
 # Start uWSGI
 ENTRYPOINT ["/code/docker-entrypoint.sh"]
--- mysite/settings.py
+++ mysite/settings.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+DATABASE_URL=os.environ.get('DATABASE_URL')
 
 
 # Quick-start development settings - unsuitable for production
@@ -57,12 +59,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#	'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+DATABASES=dict()
+DATABASES["default"]=dj_database_url.parse(DATABASE_URL)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
